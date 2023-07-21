85b8271afad8b8f044c095dad3082fb2fe7cf964
Jeniffer Carvalho <jenicarvalho@live.com>
2023-07-21 18:26:30 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import { GlobalStyle, Main, Settings } from "./global";
+import Sales from "./pages/Sales";
+
+function App() {
+  return (
+    <>
+      <Main>
+        <Header />
+        <Settings>
+          <Sales />
+        </Settings>
+      </Main>
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
