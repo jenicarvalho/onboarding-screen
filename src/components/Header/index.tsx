e94f923f05aa4ab3f39171ab6dfa81896b4b3364
Jeniffer Carvalho <jenicarvalho@live.com>
2023-07-21 18:25:40 -0300

    created component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { Container } from "./styles";
+import logo from '../../assets/logo.svg';
+import Nav from "../Nav";
+import Button from "../Button";
+
+const Header = () => {
+  return (
+    <Container>
+      <img src={logo} alt="logo" />
+      <Nav />
+      <Button>
+        Next
+      </Button>
+    </Container>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  height: 109px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 42px 0 23px;
+`;
