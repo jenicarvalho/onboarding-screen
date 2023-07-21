f3452a739bd7c5a17523cb4c5f5fc3361c108b83
Jeniffer Carvalho <jenicarvalho@live.com>
2023-07-21 19:11:21 -0300

    makes stages dynamic

--- src/components/Stages/index.tsx
+++ src/components/Stages/index.tsx
@@ -1,58 +1,43 @@
 import { Container, Item, Switch, Content, Slider } from './styles';
 
-const Stages = () => {
+type Props = {
+  selectedType: string;
+}
+
+const Stages = ({ selectedType }: Props) => {
+  const stagesByFilter: Record<string, string[]> = {
+    Basic: ["Lead", "Submitted"],
+    Advanced: ["Lead", "RFP In Progress", "Submitted"],
+    Custom: ["Lead", "RFP In Progress", "Submitted", "Closed"],
+    Expert: ["Lead", "RFP In Progress", "Submitted", "Won", "Lost", "Closed"],
+  };
+
+  const stagesToShow = stagesByFilter[selectedType] || [];
+
   return (
     <Container>
-      <Item lighter='var(--lighterPurple)' darker='var(--darkerPurple)'>
-        <strong>Lead</strong>
-        <Switch>
-          <input type="checkbox" id="lead" />
-          <Slider light='var(--lightPurple)' color='var(--purple)' />
-        </Switch>
-        <Content light='var(--lightPurple)' lighter='var(--lighterPurple)' />
-      </Item>
-      <Item lighter='var(--lighterOrange)' darker='var(--darkerOrange)'>
-        <strong>Lead</strong>
-        <Switch>
-          <input type="checkbox" id="lead" />
-          <Slider light='var(--lightOrange)' color='var(--orange)' />
-        </Switch>
-        <Content light='var(--lightOrange)' lighter='var(--lighterOrange)' />
-      </Item>
-      <Item lighter='var(--lighterBlue)' darker='var(--darkerBlue)'>
-        <strong>Lead</strong>
-        <Switch>
-          <input type="checkbox" id="lead" />
-          <Slider light='var(--lightBlue)' color='var(--blue)' />
-        </Switch>
-        <Content light='var(--lightBlue)' lighter='var(--lighterBlue)' />
-      </Item>
-      <Item lighter='var(--lighterGreen)' darker='var(--darkerGreen)'>
-        <strong>Lead</strong>
-        <Switch>
-          <input type="checkbox" id="lead" />
-          <Slider light='var(--lightGreen)' color='var(--green)' />
-        </Switch>
-        <Content light='var(--lightGreen)' lighter='var(--lighterGreen)' />
-      </Item>
-      <Item lighter='var(--lighterPink)' darker='var(--darkerPink)'>
-        <strong>Lead</strong>
-        <Switch>
-          <input type="checkbox" id="lead" />
-          <Slider light='var(--lightPink)' color='var(--pink)' />
-        </Switch>
-        <Content light='var(--lightPink)' lighter='var(--lighterPink)' />
-      </Item>
-      <Item lighter='var(--lighterDisabled)' darker='var(--darkerDisabled)'>
-        <strong>Lead</strong>
-        <Switch>
-          <input type="checkbox" id="lead" />
-          <Slider light='var(--lightDisabled)' color='var(--disabled)' />
-        </Switch>
-        <Content light='var(--lightDisabled)' lighter='var(--lighterDisabled)' />
-      </Item>
+      {stagesToShow.map((stage) => (
+        <Item key={stage} color={stageColorMap[stage]}>
+          <strong>{stage}</strong>
+          <Switch>
+            <input type="checkbox" title={stage} />
+            <Slider color={stageColorMap[stage]} />
+          </Switch>
+          <Content color={stageColorMap[stage]} />
+        </Item>
+      ))}
     </Container>
   );
 }
+
+const stageColorMap: Record<string, 'Purple' | 'Orange' | 'Blue' | 'Green' | 'Pink' | 'Disabled'> = {
+  Lead: 'Purple',
+  'RFP In Progress': 'Orange',
+  Submitted: 'Blue',
+  Won: 'Green',
+  Lost: 'Pink',
+  Closed: 'Disabled',
+};
+
 
 export default Stages;
